6bf8f01b76e66a0f6073bf36722bc144984d5340
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-02 15:05:30 +0100

    Added pipe to convert date to text stating how much time passed

---
+++ src/app/pipes/posted-ago.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PostedAgoPipe } from './posted-ago.pipe';
+
+describe('PostedAgoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PostedAgoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
