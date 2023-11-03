70d23c37ccd8f0761f77d941fe976ca6f1adfcf7
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-03 14:15:25 +0100

    Added loading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TagsService } from './services/tags.service';
+import { RetryConfig, debounce, delay, interval, retry, retryWhen } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blogsite';
+
+  isAPIAvailable: boolean = false;
+  constructor(private blogTagsService: TagsService) {}
+
+  ngOnInit(): void {
+
+    let retryConfig : RetryConfig = {count: 50, delay: 5000, resetOnSuccess : false}
+
+    this.blogTagsService
+      .getAllTags()
+      .pipe(retry(retryConfig), )
+      .subscribe(
+        {
+          error:() => {
+            this.isAPIAvailable = false;
+          },
+
+          complete: () => {
+            this.isAPIAvailable = true;
+          }
+        }
+      );
+  }
 }
