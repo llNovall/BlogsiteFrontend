6f9f6e12b3a7c03517e9f48ce0edd23d9b49586f
llNovall <25434267+llNovall@users.noreply.github.com>
2023-11-03 14:20:24 +0100

    Updated homepage with material

--- src/app/pages/frontend/homepage/homepage.component.ts
+++ src/app/pages/frontend/homepage/homepage.component.ts
@@ -5,22 +5,30 @@ import { BlogsService } from 'src/app/services/blogs.service';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
+  featuredBlogs: Blog[] = [];
+  blogs: Blog[] = [];
 
-  blogs : Blog[] = [];
+  constructor(private blogsService: BlogsService) {}
 
-  constructor (private blogsService: BlogsService){}
   ngOnInit(): void {
-    this.blogsService.getAllBlogs().subscribe(blogs => { this.blogs =blogs; });
+    this.blogsService.getAllBlogs().subscribe({
+      next: (blogs) => {
+        this.blogs = blogs;
+        this.featuredBlogs = blogs.filter((c) => c.isFeatured == true);
+      },
+
+      error: () => {},
+    });
   }
 
-  isThereAFeaturedBlog(): boolean{
-    return this.blogs.some(c=> c.isFeatured);
+  isThereAFeaturedBlog(): boolean {
+    return this.blogs.some((c) => c.isFeatured);
   }
 
-  getBlogId(index : number, blog : Blog){
+  getBlogId(index: number, blog: Blog) {
     return blog.id;
   }
 }
